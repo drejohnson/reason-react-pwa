f1376d572af21d7c790f817379dd61695a5e4355
DeAndre <dre@phreshr.com>
2018-05-29 23:46:51 +0400

    update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const history = require('connect-history-api-fallback');
 const convert = require('koa-connect');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ScriptExtHtmlPlugin = require('script-ext-html-webpack-plugin');
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
@@ -12,12 +13,21 @@ const WebpackBar = require('webpackbar');
 const isProd = process.env.NODE_ENV === 'production';
 
 module.exports = {
-  entry: './src/Index.bs.js',
+  entry: {
+    app: './src/Index.bs.js',
+  },
   mode: isProd ? 'production' : 'development',
   output: {
     path: path.resolve(__dirname, './dist'),
     filename: !isProd ? '[name].js' : '[name].[chunkhash:8].js',
     publicPath: '/'
+  },
+  resolve: {
+    extensions: ['.bs.js', '.js', '.scss', '.sass', '.css', '.html'],
+    alias: {
+      '~': './src',
+    },
+    modules: ['./src', 'node_modules'],
   },
   module: {
     rules: [
@@ -28,6 +38,23 @@ module.exports = {
           'css-loader',
           'postcss-loader'
         ]
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 8192, // return DataURL if image size <= 8KB
+              name: 'assets/[name].[ext]',
+              fallback: 'file-loader' // use file loader for size > 8KB
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: ['file-loader']
       }
     ]
   },
@@ -39,74 +66,106 @@ module.exports = {
       chunkFilename: !isProd ? '[id].css' : '[id].[hash].css'
     }),
     new HtmlWebpackPlugin({
+      minify: {
+        collapseWhitespace: isProd,
+        removeComments: isProd,
+        removeRedundantAttributes: isProd,
+        removeScriptTypeAttributes: isProd,
+        removeStyleLinkTypeAttributes: isProd,
+        sortAttributes: isProd,
+        sortClassName: isProd,
+        useShortDoctype: isProd,
+        minifyCSS: isProd,
+        minifyJS: isProd,
+        caseSensitive: isProd,
+      },
+      hash: isProd,
       inject: true,
       template: './public/index.html'
+    }),
+    new ScriptExtHtmlPlugin({
+      defaultAttribute: 'defer',
+      module: 'app',
     }),
     new WebpackBar(),
     ...(isProd
       ? [
-          new WorkboxPlugin.GenerateSW({
-            swDest: 'sw.js',
-            clientsClaim: true,
-            skipWaiting: true,
-            // Exclude images from the precache
-            exclude: [/\.(?:png|jpg|jpeg|svg)$/],
-            // Define runtime caching rules.
-            runtimeCaching: [
-              {
-                urlPattern: /^(https?.*)/,
-                handler: 'networkFirst',
-                options: {
-                  cacheName: 'cache-https',
-                  expiration: {
-                    maxEntries: 50
-                  },
-                  networkTimeoutSeconds: 3
-                }
-              },
-              {
-                urlPattern: new RegExp(
-                  'https://fonts.(?:googleapis|gstatic).com/(.*)'
-                ),
-                // Apply a cache-first strategy.
-                handler: 'cacheFirst',
-                options: {
-                  cacheName: 'googleapis',
-                  expiration: {
-                    maxEntries: 50
-                  }
-                }
-              },
-              {
-                // Match any request ends with .png, .jpg, .jpeg or .svg.
-                urlPattern: /\.(?:png|jpg|jpeg|svg)$/,
-
-                // Apply a cache-first strategy.
-                handler: 'cacheFirst',
-
-                options: {
-                  cacheName: 'images-cache',
-                  // Only cache 10 images.
-                  expiration: {
-                    maxEntries: 60,
-                    maxAgeSeconds: 30 * 24 * 60 * 60 // 30 Days
-                  },
-                  cacheableResponse: {
-                    statuses: [0, 200]
-                  }
-                }
-              },
-              {
-                urlPattern: /\.(?:js|css)$/,
-                handler: 'staleWhileRevalidate',
-                options: {
-                  cacheName: 'static-resources'
+        new WorkboxPlugin.GenerateSW({
+          swDest: 'sw.js',
+          clientsClaim: true,
+          skipWaiting: true,
+          // Exclude images from the precache
+          exclude: [/\.(?:png|jpg|jpeg|svg)$/],
+          // Define runtime caching rules.
+          runtimeCaching: [
+            {
+              urlPattern: /^(https?.*)/,
+              handler: 'networkFirst',
+              options: {
+                cacheName: 'cache-https',
+                expiration: {
+                  maxEntries: 50
+                },
+                networkTimeoutSeconds: 3
+              }
+            },
+            {
+              urlPattern: new RegExp(
+                'https://fonts.googleapis.com/(.*)'
+              ),
+              // Apply a cache-first strategy.
+              handler: 'cacheFirst',
+              options: {
+                cacheName: 'googleapis',
+                expiration: {
+                  maxEntries: 50
                 }
               }
-            ]
-          }),
-          new BundleAnalyzerPlugin({ openAnalyzer: false })
-        ]
+            },
+            {
+              // Match any request ends with .png, .jpg, .jpeg or .svg.
+              urlPattern: /\.(?:png|jpg|jpeg|svg)$/,
+
+              // Apply a cache-first strategy.
+              handler: 'cacheFirst',
+
+              options: {
+                cacheName: 'images-cache',
+                // Only cache 10 images.
+                expiration: {
+                  maxEntries: 60,
+                  maxAgeSeconds: 30 * 24 * 60 * 60 // 30 Days
+                },
+                cacheableResponse: {
+                  statuses: [0, 200]
+                }
+              }
+            },
+            {
+              urlPattern: /\.(?:js|css)$/,
+              handler: 'staleWhileRevalidate',
+              options: {
+                cacheName: 'static-resources'
+              }
+            },
+            {
+              urlPattern: /.*(?:googleapis)\.com.*$/,
+              handler: 'staleWhileRevalidate',
+              options: {
+                cacheName: 'googleapis-cache'
+              }
+            },
+            {
+              urlPattern: /.*(?:gstatic)\.com.*$/,
+              handler: 'staleWhileRevalidate',
+              options: {
+                cacheName: 'gstatic-cache'
+              }
+            }
+          ]
+        }),
+        // new BundleAnalyzerPlugin({ openAnalyzer: false })
+      ]
       : [])
   ],
   optimization: {
@@ -135,6 +194,7 @@ module.exports = {
 if (!isProd) {
   module.exports.serve = {
     content: [__dirname],
+    hot: true,
     add: (app, middleware, options) => {
       const historyOptions = {
         // ... see: https://github.com/bripkens/connect-history-api-fallback#options
